186daff9229f02c5fdc7583f68f8e285b4ba5439
Mark Miyashita <negativetwelve@gmail.com>
2013-08-28 19:09:54 -0700

    brian is really dumb

--- happiness.py
+++ happiness.py
@@ -5,6 +5,10 @@ FIRST_CHOICE = 10
 SECOND_CHOICE = 1
 INFINITY = -100000000000000
 
+MAX_NUM_PER_LAB = 5
+MAX_NUM_PER_DISC = 1
+MAX_NUM_PER_OH = 3
+
 class LabAssistant(object):
 
     def __init__(self, first_choices, second_choices, cant_make, preferred_tas, num_labs, num_discussions, num_office_hours):
@@ -57,7 +61,15 @@ def parsed(line):
     second_choices = str(second_labs + ', ' + second_discs).split(', ')
     cant_make = str(cant_labs + ', ' + cant_discs).split(', ')
     preferred_tas = preferred_tas.split(', ')
-    return [first_choices, second_choices, cant_make, preferred_tas, int(num_labs), int(num_discussions), int(num_oh)]
+
+    first_labs = first_labs.split(', ')
+    second_labs = second_labs.split(', ')
+
+    first_discs = first_discs.split(', ')
+    second_discs = second_discs.split(', ')
+    ohs = oh_times.split(', ')
+
+    return [first_choices, second_choices, cant_make, preferred_tas, int(num_labs), int(num_discussions), int(num_oh)], [first_labs + second_labs, first_discs + second_discs, ohs]
 
 def read_file(filename):
     la = []
@@ -74,17 +86,22 @@ def read_file(filename):
             if num > 2:
                 break
             per_line = parsed(line)
+            labs.update(set(per_line[1][0]))
+            discs.update(set(per_line[1][1]))
+            office_hours.update(set(per_line[1][2]))
             print per_line
-            la.append(LabAssistant(*per_line))
+            la.append(LabAssistant(*per_line[0]))
             num += 1
-    return la
+    return la, list(map(list, (labs, discs, office_hours)))
 
-def maximize(lab_assistants):
+def maximize(lab_assistants, labs, discs, ohs):
+    
+
     total_happy = 0
     for la in lab_assistants:
         total_happy += la.happiness
-    print total_happy
     return total_happy
 
 if __name__ == '__main__':
-    maximize(read_file('responses.csv'))
+    la, (labs, discs, ohs) = read_file('responses.csv')
+    print maximize(la, labs, discs, ohs)
